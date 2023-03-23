cdf7d2ea315d72650fd3537f919af4e4a6f5b79d
mamaninang <63132161+mamaninang@users.noreply.github.com>
2023-03-23 13:37:17 -0700

    Created TopItem Component

--- frontend/src/components/MainDisplay.js
+++ frontend/src/components/MainDisplay.js
@@ -6,118 +6,23 @@ import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import data from '../sample_data/top_tracks_short.json';
 import {getDuration} from '../functions';
+import TopItem from '../components/TopItem';
 
 function MainDisplay() {
-    const [expanded, setExpanded] = React.useState(false);
+    const NUM_ITEMS_IN_PAGE = 10;
+    // const [expanded, setExpanded] = React.useState(false);
+    const [page, setPage] = React.useState(1);
+    const [offset, setOffset] = React.useState(NUM_ITEMS_IN_PAGE * (page-1));
 
-    let panelNum = 1;
-    let panelName = `panel${panelNum}`;
-
-    const handleChange =
-    (panel) => (event, isExpanded) => {
-        setExpanded(isExpanded ? panel : false);
-        panelNum++;
-    };
-
-    const getItems = () => data.items.map(item => 
-        <Accordion expanded={expanded === panelName} onChange={handleChange(panelName)}>
-        <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1bh-content"
-            id="panel1bh-header"
-        >
-            <Typography sx={{ flex:1 }}>
-            {item['name']}
-            </Typography>
-            <Typography sx={{ flex:1, color: 'text.secondary' }}>{item['name']} by {item['artists'][0]['name']}</Typography>
-            <Typography sx={{ flex:1, width: '33%', color: 'text.secondary' }}>
-            {getDuration(item['duration_ms'])}
-            </Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-            <Typography>
-            Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat.
-            Aliquam eget maximus est, id dignissim quam.
-            </Typography>
-        </AccordionDetails>
-        </Accordion>
+    const getItems = () => 
+        // data.items.map((item, index) => 
+        data.items.slice(offset, offset + NUM_ITEMS_IN_PAGE).map((item, index) => 
+            <TopItem item={item} number={offset+index+1}/>
     );
   
     return (
         <div>
             {getItems()}
-            {/* <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
-            <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1bh-content"
-                id="panel1bh-header"
-            >
-                <Typography sx={{ width: '33%', flexShrink: 0 }}>
-                {items[0]['name']}
-                </Typography>
-                <Typography sx={{ color: 'text.secondary' }}>{items[0]['name']} by {items[0]['artists'][0]['name']}</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-                <Typography>
-                Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat.
-                Aliquam eget maximus est, id dignissim quam.
-                </Typography>
-            </AccordionDetails>
-            </Accordion>
-            <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
-            <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel2bh-content"
-                id="panel2bh-header"
-            >
-                <Typography sx={{ width: '33%', flexShrink: 0 }}>Users</Typography>
-                <Typography sx={{ color: 'text.secondary' }}>
-                You are currently not an owner
-                </Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-                <Typography>
-                Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus,
-                varius pulvinar diam eros in elit. Pellentesque convallis laoreet
-                laoreet.
-                </Typography>
-            </AccordionDetails>
-            </Accordion>
-            <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
-            <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel3bh-content"
-                id="panel3bh-header"
-            >
-                <Typography sx={{ width: '33%', flexShrink: 0 }}>
-                Advanced settings
-                </Typography>
-                <Typography sx={{ color: 'text.secondary' }}>
-                Filtering has been entirely disabled for whole web server
-                </Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-                <Typography>
-                Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit
-                amet egestas eros, vitae egestas augue. Duis vel est augue.
-                </Typography>
-            </AccordionDetails>
-            </Accordion>
-            <Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
-            <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel4bh-content"
-                id="panel4bh-header"
-            >
-                <Typography sx={{ width: '33%', flexShrink: 0 }}>Personal data</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-                <Typography>
-                Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit
-                amet egestas eros, vitae egestas augue. Duis vel est augue.
-                </Typography>
-            </AccordionDetails>
-            </Accordion> */}
         </div>
     )
 }
